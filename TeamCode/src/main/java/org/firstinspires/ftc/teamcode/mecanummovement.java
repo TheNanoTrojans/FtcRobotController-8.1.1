04d4532d0d467ec865006d51e1c46775dae78721
TheNanoTrojans <nanotrojansftc@gmail.com>
2022-12-25 15:13:44 -0600

    autoCommit 2022 1Su.151344

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanummovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanummovement.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
