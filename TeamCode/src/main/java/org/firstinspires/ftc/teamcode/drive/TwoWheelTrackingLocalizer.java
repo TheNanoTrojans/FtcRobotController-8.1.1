69f7cc22e69a220cdff9b5cf5f9428a5292fd7dd
TheNanoTrojans <nanotrojansftc@gmail.com>
2022-12-29 18:13:44 -0600

    autoCommit 2022 1Th.181344

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -31,7 +31,7 @@ import java.util.List;
  *    |              |
  *    |              |
  *    \--------------/
- * 1.04089361 + 1.03252033 +
+ *
  */
 public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 8192;
@@ -45,7 +45,7 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double PERPENDICULAR_Y = -8.5;
 
     public static double X_MULTIPLIER = 1.035; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
+    public static double Y_MULTIPLIER = 1.042; // Multiplier in the Y direction
 
     // Parallel/Perpendicular to the forward axis
     // Parallel wheel is parallel to the forward axis
