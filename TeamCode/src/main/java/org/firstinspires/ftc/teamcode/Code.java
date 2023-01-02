904b77acc93e6a477913b92fa408e478199dcea3
TheNanoTrojans <nanotrojansftc@gmail.com>
2023-01-01 22:47:37 -0600

    autoCommit 2023 0Su.224737

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code.java
@@ -3,10 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
+@Autonomous(name = "Code", group = "Autonomous")
 public class Code extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -14,6 +15,7 @@ public class Code extends LinearOpMode {
 
         Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
                 .splineTo(new Vector2d(10,10), Math.toRadians(0))
+                .splineTo(new Vector2d(0,0), Math.toRadians(0))
                         .build();
         waitForStart();
         if(isStopRequested()) return;
