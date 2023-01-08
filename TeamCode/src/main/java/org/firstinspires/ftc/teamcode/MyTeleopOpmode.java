f282f9e946aa2306081bcdc56d227dfe189998e7
TheNanoTrojans <nanotrojansftc@gmail.com>
2023-01-07 20:43:44 -0600

    autoCommit 2023 0Sa.204344

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleopOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleopOpmode.java
@@ -47,17 +47,34 @@ public class MyTeleopOpmode extends LinearOpMode {
         TwoTrackingWheelLocalizer myLocalizer = new TwoWheelTrackingLocalizer(hardwareMap, drive);
 
         // Set your initial pose to x: 10, y: 10, facing 90 degrees
-        myLocalizer.setPoseEstimate(new Pose2d(10, 10, Math.toRadians(90)));
-        Trajectory myTrajectory = drive.trajectoryBuilder(myLocalizer.getPoseEstimate())
-                .strafeTo(new Vector2d(8,65))
-                .build();
+
 
 
         waitForStart();
 
+
+            // Retrieve your pose
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+            Pose2d myPose = myLocalizer.getPoseEstimate();
+
+            telemetry.addData("x", myPose.getX());
+            telemetry.addData("y", myPose.getY());
+            telemetry.addData("heading", myPose.getHeading());
+
+            // Insert whatever teleop code you're using
         while(opModeIsActive()) {
             // Make sure to call myLocalizer.update() on *every* loop
             // Increasing loop time by utilizing bulk reads and minimizing writes will increase your odometry accuracy
+            myLocalizer.setPoseEstimate(new Pose2d(10, 10, Math.toRadians(90)));
+            Trajectory myTrajectory = drive.trajectoryBuilder(myLocalizer.getPoseEstimate())
+                    .strafeTo(new Vector2d(8,65))
+                    .build();
             myLocalizer.update();
             frontLeft.setDirection(DcMotor.Direction.REVERSE);
             //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -130,28 +147,14 @@ public class MyTeleopOpmode extends LinearOpMode {
             if (gamepad2.a){
                 intakeClaw.setPosition(0.2);
             }
-            if (gamepad1.a){
+            if (gamepad1.y){
                 drive.followTrajectoryAsync(myTrajectory);
                 if(gamepad1.b){
                     stop();
                 }
 
             }
-            // Retrieve your pose
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
-                            -gamepad1.right_stick_x
-                    )
-            );
-            Pose2d myPose = myLocalizer.getPoseEstimate();
-
-            telemetry.addData("x", myPose.getX());
-            telemetry.addData("y", myPose.getY());
-            telemetry.addData("heading", myPose.getHeading());
-
-            // Insert whatever teleop code you're using
         }
+
     }
 }
