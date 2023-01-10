df6fe26369fe9fddc773ad697da25cd91025446d
TheNanoTrojans <nanotrojansftc@gmail.com>
2023-01-09 19:43:44 -0600

    autoCommit 2023 0Mo.194344

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutoRightBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutoRightBlue.java
@@ -5,6 +5,7 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -16,7 +17,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+@Autonomous(name = "MainAutoRightBlue")
 public class MainAutoRightBlue extends LinearOpMode {
     private SleeveDetection sleeveDetection;
     private OpenCvCamera camera;
