af0f18028b79952d22c09b52a3561d2d8e2ce681
TheNanoTrojans <nanotrojansftc@gmail.com>
2023-01-19 20:03:44 -0600

    autoCommit 2023 0Th.200344

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
@@ -94,7 +94,7 @@ public class AutoLeftRed extends LinearOpMode {
                     .lineToLinearHeading(new Pose2d(11.5 , -62, Math.toRadians(295.77)))
                     .build();
             Trajectory myTrajectory3 = drive.trajectoryBuilder(myTrajectory1.end())
-                    .lineTo(new Vector2d(10.45 ,-67.79))
+                    .lineTo(new Vector2d(11.45 ,-67.79))
                     .build();
             Trajectory myTrajectory5 = drive.trajectoryBuilder(myTrajectory3.end())
                     .strafeTo(new Vector2d(11,-63))
