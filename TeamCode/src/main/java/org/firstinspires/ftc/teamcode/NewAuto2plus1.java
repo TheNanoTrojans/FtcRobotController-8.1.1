003ade67de8642ef7ad03f9de2cce212988d2df7
TheNanoTrojans <nanotrojansftc@gmail.com>
2023-02-21 18:39:06 -0600

    autoCommit 2023 0Tu.183906

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto2plus1.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Autonomous
+public class NewAuto2plus1 extends LinearOpMode {
+    private DcMotor lsLeft;
+    private DcMotor lsRight;
+    private DcMotor lsTurn;
+    private DcMotor lsIntake;
+    private Servo intakeClaw;
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    @Override
+    public void runOpMode(){
+           lsLeft = hardwareMap.dcMotor.get("lsLeft");
+           lsRight = hardwareMap.dcMotor.get("lsRight");
+           lsTurn = hardwareMap.dcMotor.get("lsTurn");
+           lsIntake = hardwareMap.dcMotor.get("lsIntake");
+           intakeClaw = hardwareMap.servo.get("intakeClaw");
+           lsLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+           lsTurn.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+           if(opModeIsActive()){
+               Trajectory traj1 = drive.trajectoryBuilder(new Pose2d(-35,70,Math.toRadians(-90)))
+                       .lineTo(new Vector2d(-39,67))
+                       .build();
+               Trajectory myTraj1 = drive.trajectoryBuilder(traj1.end())
+                       .lineTo(new Vector2d(-39,18))
+                       .build();
+               Trajectory myTraj2 = drive.trajectoryBuilder(myTraj1.end())
+                       .lineToLinearHeading(new Pose2d(-61,18,Math.toRadians(180)))
+                       .build();
+               Trajectory myTraj3 = drive.trajectoryBuilder(myTraj1.end())
+                       .lineToLinearHeading(new Pose2d(26,19.5,Math.toRadians(-90)))
+                       .build();
+               Trajectory myTraj4 = drive.trajectoryBuilder(myTraj3.end())
+                       .lineToLinearHeading(new Pose2d(-40,18,Math.toRadians(180)))
+                       .build();
+               Trajectory myTraj5  = drive.trajectoryBuilder(myTraj4.end())
+                       .lineTo(new Vector2d(-61,18))
+                       .build();
+
+               drive.setPoseEstimate(new Pose2d(-35,70,Math.toRadians(-90)));
+               drive.followTrajectory(traj1);
+               drive.followTrajectory(myTraj1);
+               linearSlide(3200,1);
+               Turret(600,1);
+               sleep(2000);
+               LsIntake(300,1);
+               linearSlide(2800,1);
+               sleep(400);
+               linearSlide(2400,1);
+               intakeClaw.setPosition(0.5);
+               sleep(200);
+               Turret(0,1);
+               LsIntake(0,1);
+               sleep(200);
+               linearSlide(350,1);
+               drive.followTrajectory(myTraj2);
+               intakeClaw.setPosition(1);
+               sleep(250);
+               linearSlide(3200,1);
+               drive.followTrajectory(myTraj3);
+               LsIntake(150,1);
+               linearSlide(2800,1);
+               sleep(200);
+               linearSlide(2400,1);
+               intakeClaw.setPosition(0.4);
+               sleep(250);
+               linearSlide(0,1);
+               LsIntake(0,1);
+               Turret(0,1);
+               drive.followTrajectory(myTraj4);
+               linearSlide(280,1);
+               drive.followTrajectory(myTraj5);
+               intakeClaw.setPosition(1);
+               sleep(250);
+               linearSlide(3200,1);
+               drive.followTrajectory(myTraj3);
+               LsIntake(150,1);
+               linearSlide(2800,1);
+               sleep(200);
+               linearSlide(2400,1);
+               intakeClaw.setPosition(0.4);
+               sleep(250);
+
+
+
+
+           }
+
+
+    }
+    private void linearSlide(int TargetPosition, double Speed){
+        lsLeft.setTargetPosition(TargetPosition);
+        lsRight.setTargetPosition(TargetPosition);
+        lsLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lsRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lsLeft.setPower(Speed);
+        lsRight.setPower(Speed);
+
+    }
+
+    private void Turret(int TargetPosition,double Speed){
+        lsTurn.setTargetPosition(TargetPosition);
+        lsTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lsTurn.setPower(Speed);
+    }
+    private void LsIntake(int TargetPos, double Speed){
+        lsIntake.setTargetPosition(TargetPos);
+        lsIntake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lsIntake.setPower(Speed);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class TeleOpMain extends LinearOpMode {
+    private DcMotor lsLeft;
+    private DcMotor lsRight;
+    private DcMotor lsTurn;
+    private DcMotor lsIntake;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private Servo intakeClaw;
+    @Override
+    public void runOpMode(){
+        lsLeft = hardwareMap.dcMotor.get("lsLeft");
+        lsRight = hardwareMap.dcMotor.get("lsRight");
+        lsTurn = hardwareMap.dcMotor.get("lsTurn");
+        lsIntake = hardwareMap.dcMotor.get("lsIntake");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        intakeClaw = hardwareMap.servo.get("intakeClaw");
+        lsLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        lsTurn.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        while(opModeIsActive()){
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+            lsLeft.setPower(-gamepad2.left_stick_y);
+            lsRight.setPower(-gamepad2.left_stick_y);
+            lsTurn.setPower(-gamepad2.right_stick_y);
+            lsIntake.setPower(-gamepad2.right_stick_y);
+        }
+    }
+}
